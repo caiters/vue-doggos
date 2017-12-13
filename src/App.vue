775bec75a8d7a61bd310c04f8907a71fc4030aaf
Caitlin <caitlin.bailey@webadvanced.com>
2017-12-13 12:38:10 -0800

    doggo

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <doggos></doggos>
+</template>
+
+<script>
+import doggos from './components/doggos'
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  },
+  components: {
+    doggos: doggos
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/doggos.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+<img :src="doggo" />
+<p>a good pupper</p>
+</div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            doggo: '',
+            thing: 'asdfasd'
+        }
+    },
+    mounted() {
+        this.getDoggo();
+    },
+    methods: {
+        getDoggo: function(){
+            let app = this;
+            fetch('https://dog.ceo/api/breed/pug/images/random', { method: 'GET'}).then((res) => res.json()).then(function(json){ app.doggo = json.message; })
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+div { text-align: center; font-family: sans-serif; }
+img { max-width: 300px; }
+</style>
